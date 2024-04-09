01c0982102fa1c8a05fc24d06f279d8ba15a56d6
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-04-09 18:25:30 +0530

    update

--- test1.sql
+++ test1.sql
@@ -9,11 +9,3 @@ insert into players.cricketers(playerid, name) values (1, "Virat Kohli");
 insert into players.cricketers(playerid, name) values (2, "Rsohit Sharma");
 
 select * from players.cricketers;
-
--- comment
-# comment
-/* 
-comment
-*/
-CREATE DATABASE temp1; -- creating a databse  
-DROP DATABASE temp1; -- dropping/deleting the database
