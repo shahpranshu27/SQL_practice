bd9785277888e7560fb9ef915c10881a6cbec1a4
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-04-09 18:44:23 +0530

    Add files via upload

---
+++ college.sql
@@ -0,0 +1,25 @@
+-- comment
+# comment
+/* 
+comment
+*/
+CREATE DATABASE temp1; -- creating a databse  
+DROP DATABASE temp1; -- dropping/deleting the database
+
+CREATE DATABASE college;
+
+USE college; -- this command USE is used when whatever we want to do, we want to do in that particular database only
+
+-- creating a table in the database
+CREATE TABLE student(
+id INT PRIMARY KEY,
+name VARCHAR(55),
+age INT NOT NULL
+);
+
+-- inserting the values in the table created
+INSERT into student values(1, "Pranshu", 21);
+INSERT into student values(2, "Siddh", 22);
+
+-- printing the table
+SELECT * FROM student;
