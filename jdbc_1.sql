cac53dc1e7ff47ed03b65bc792b448fd130c810d
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-04-12 17:01:49 +0530

    Add files via upload

---
+++ jdbc_1.sql
@@ -0,0 +1,16 @@
+create database jdbc_db;
+
+use jdbc_db;
+
+create table register(
+name varchar(50),
+email varchar(50),
+password varchar(50),
+gender varchar(10),
+city varchar(50)
+);
+
+desc register;
+
+select * from register;
+
